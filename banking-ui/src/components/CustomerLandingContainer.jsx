045d1a3985f5e8b8e002333c599a5bff0f8eed90
Hari Govind <hargovi1@in.ibm.com>
2019-12-11 14:03:20 +0530

    UI v2.0

---
+++ banking-ui/src/components/CustomerLandingContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class CustomerLandingContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CustomerLandingContainer;
